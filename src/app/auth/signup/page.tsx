761908f5968d22833421e101b2d7b5c76c47e170
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-03-12 11:21:14 +0545

    next ui step, signup

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,106 +1,85 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { FormControl, FormGroup } from "@mui/material";
-import MegaMedLogo from "./../../../ui/MegaMedLogo";
+import react from 'react';
+import { Input, Button } from '@nextui-org/react';
+import MegaMedLogo from '@/ui/MegaMedLogo';
+import Link from 'next/link';
+import { useState } from 'react';
+import { FaRegEye, FaRegEyeSlash } from 'react-icons/fa';
 
-export default function SignUp() {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      data: data,
-      email: data.get("email"),
-      password: data.get("password"),
-      fullName: data.get("fullName"),
-      phoneNumber: data.get("phoneNumber"),
-    });
-  };
+export default function SignupPage() {
+  const [isVisible, setIsVisible] = useState(false);
+  // const toogleVisibility = () => setIsVisible(!isVisible);
 
   return (
-    <Container component="main" maxWidth="xs">
-      <Box className="my-16 w-full flex flex-col items-center space-y-6  border border-gray-300 rounded p-8">
+    <div className="flex items-center justify-center h-fit">
+      <div className="flex flex-col items-center gap-6 border-2 rounded-md radius p-8 m-4 w-96">
         <MegaMedLogo />
-
-        <Typography
-          component="h1"
-          variant="body1"
-          className="text-center text-gray-400"
-        >
-          Sign up to unlock your potential in medical enrance exams.
-        </Typography>
-        <FormControl
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{ mt: 3, width: "100%" }}
-          className="flex flex-col space-y-4"
-        >
-          <FormGroup>
-            <TextField
-              required
-              fullWidth
-              id="fullName"
-              label="Full Name"
-              name="fullName"
-              type="text"
-              autoComplete="family-name"
-              size="small"
-              className="mb-4 bg-gray-50 border-gray-300 rounded"
-            />
-            <TextField
-              required
-              fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              type="email"
-              autoComplete="email"
-              size="small"
-              className="mb-4 bg-gray-50 border-gray-300 rounded"
-            />
-            <TextField
-              required
-              fullWidth
-              id="phoneNumber"
-              label="Phone Number"
-              name="phoneNumber"
-              type="tel"
-              autoComplete="tel"
-              size="small"
-              className="mb-4 bg-gray-50 border-gray-300 rounded"
-            />
-            <TextField
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="new-password"
-              size="small"
-              className="mb-4 bg-gray-50 border-gray-300 rounded"
-            />
-          </FormGroup>
-          <Button type="submit" fullWidth variant="contained">
-            Sign Up
+        <h1 className="text-center">
+          Sign up to unlock your potential in medical
+          enrance exams.
+        </h1>
+        <form className="flex flex-col gap-3 w-full">
+          <Input
+            type="text"
+            label="Full Name"
+            size="sm"
+            radius="lg"
+            isRequired={true}
+          />
+          <Input
+            type="email"
+            label="Email Address"
+            size="sm"
+            radius="lg"
+            isRequired={true}
+          />
+          <Input
+            type="tel"
+            label="Mobile Number"
+            size="sm"
+            radius="lg"
+            isRequired={true}
+          />
+          <Input
+            type={isVisible ? 'text' : 'password'}
+            endContent={
+              isVisible ? (
+                <FaRegEyeSlash
+                  onClick={() => setIsVisible(false)}
+                  className="cursor-pointer text-gray-500 text-xl"
+                />
+              ) : (
+                <FaRegEye
+                  onClick={() => setIsVisible(true)}
+                  className="cursor-pointer text-gray-500 text-xl"
+                />
+              )
+            }
+            label="Password"
+            size="sm"
+            radius="lg"
+            isRequired={true}
+          />
+          <Button
+            type="submit"
+            color="primary"
+            className="rounded-3xl"
+          >
+            Signup
           </Button>
-          <Grid container justifyContent="center">
-            <Grid item>
-              Already have an account?
-              <Link href="#" variant="body2">
-                Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </FormControl>
-      </Box>
-    </Container>
+        </form>
+
+        <p>
+          Already have an account?{' '}
+          <Link
+            href="#to /auth/login"
+            className="text-blue-500 hover:underline"
+          >
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
   );
 }
