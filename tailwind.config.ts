5fd5e4cda6587c3e3030502638f4541230749de2
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-03-15 17:07:18 +0545

    tailwind config fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,16 +3,13 @@ import { nextui } from '@nextui-org/react';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial':
-          'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
@@ -21,4 +18,5 @@ const config: Config = {
   darkMode: 'class',
   plugins: [nextui()],
 };
+
 export default config;
