025c8a40c1b91065123fef99e4b855ba032e1673
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-03-19 19:43:04 +0545

    minor changes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,9 @@ import type { Config } from 'tailwindcss';
 const config = {
   darkMode: ['class'],
   content: [
-    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-
+    './src/**/*.{ts,tsx,js,jsx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   prefix: '',
   theme: {
