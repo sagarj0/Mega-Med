73aa962d312942562fd04870895648f8a9f29d30
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-03-25 06:59:20 +0545

    minor changes

--- src/app/admin/manage/questions/page.tsx
+++ src/app/admin/manage/questions/page.tsx
@@ -11,31 +11,6 @@ export default function Page() {
       <Link href="/admin/manage/questions/addQuestion">
         <Button> Add </Button>
       </Link>
-
-      <h1 className="m-8">Radio form </h1>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log('form submitted');
-        }}
-      >
-        {/* <RadioGroup name="radio" isRequired={true}>
-          <Radio value="1">Option 1</Radio>
-          <Radio value="2">Option 2</Radio>
-          <Radio value="3">Option 3</Radio>
-        </RadioGroup> */}
-        <div role="radiogroup" aria-required>
-          <label>
-            <input type="radio" name="radio" value="1" />
-            Option 1
-          </label>
-          <label>
-            <input type="radio" name="radio" value="2" />
-            Option 2
-          </label>
-        </div>
-        <Button type="submit">Submit</Button>
-      </form>
     </div>
   );
 }
