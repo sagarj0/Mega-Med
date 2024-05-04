602ed2a145289a09de8236cf213df0baf3e72aa4
sagarj0 <joshhisagar9375@gmail.com>
2024-05-04 11:13:53 +0545

    minor ui change

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,12 +3,12 @@ import Header from '@/ui/admin/header';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-full flex-col md:flex-row items-stretch md:overflow-hidden  gap-3 px-2 py-3">
-      <div className="w-full md:h-full flex-none md:w-64 h-fit ">
+    <div className="flex h-full flex-col md:flex-row  gap-3 px-2 py-3">
+      <div className="w-full md:h-[96vh]   md:w-64 h-fit ">
         <SideNav />
       </div>
-      <div className="h-[96vh] md:w-5/6">
-        <main className="w-full h-full flex flex-col items-center gap-4 ">
+      <div className="md:h-[96vh] md:w-5/6">
+        <main className="w-full h-full flex flex-col md:overflow-y-scroll items-center gap-4 ">
           <Header />
           {children}
         </main>
