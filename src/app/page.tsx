7afe0ccfb404cac730db8a0f5463e81d5024ddf3
siddhantPaudel <siddhantpaudel777@gmail.com>
2024-05-04 15:48:19 +0545

    navbar edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,16 @@
 
 import React from 'react';
 import { Button, Link } from '@nextui-org/react';
+import Navbar from '@/ui/home-page/navbar';
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-between p-16">
-      <p>This is home page</p>
+    <div className="flex flex-col justify-between p-16"> 
+     <Navbar/>
 
-      <div className="space-x-4">
-        <Link href="/auth/signup">
-          <Button color="primary">Signup</Button>
-        </Link>
-        <Link href="/auth/login">
-          <Button color="primary">Login</Button>
-        </Link>
-      </div>
+     
+    
+
     </div>
   );
 }
--- src/ui/home-page/navbar.tsx
+++ src/ui/home-page/navbar.tsx
@@ -1,5 +1,7 @@
 import react from "react"
-import Link from 'next/link';
+// import Link from 'next/link';
+import { Button, Link } from '@nextui-org/react';
+import MegaMedLogo from "../MegaMedLogo";
 
 
 interface NavLinkProps {
@@ -12,24 +14,30 @@ export default function Navbar(){
         <div className="bg-white-50/50 rounded-md p-4 flex items-center justify-between w-full">
           {/* Logo */}
           <div>
-            <Link href="/">
-              Next.js App {/* Removed the `<a>` tag here */}
-            </Link>
+           <MegaMedLogo/>
           </div>
-      
-          {/* Navigation Links */}
-          <div className="flex space-x-4">
+
+          
+          <div className="flex space-x-4 ">
             <NavLink href="/" label="Home" />
             <NavLink href="/about" label="About" />
             <NavLink href="/mentors" label="Mentors" />
             <NavLink href="/courses" label="Courses" />
           </div>
+          <div className="space-x-4  flex justify-end">
+        <Link href="/auth/signup">
+          <Button color="primary">Signup</Button>
+        </Link>
+        <Link href="/auth/login">
+          <Button color="primary">Login</Button>
+        </Link>
+      </div>
         </div>
       
     )}
 
 
-    const NavLink: React.FC<NavLinkProps> = ({ href, label }) => {
+const NavLink: React.FC<NavLinkProps> = ({ href, label }) => {
         return (
           <Link href={href as string}>
             {label}
